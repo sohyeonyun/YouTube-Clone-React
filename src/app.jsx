6639d61728fbdcdba4f114bbf28b9a4b3a62287b
sohyeonyun <wja06018@naver.com>
2022-01-02 21:55:54 +0900

    show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,26 @@
-
+import { useEffect, useState } from 'react';
 import './app.css';
+import VideoList from './components/video_list/video_list';
 
 function App() {
-  return (
-    <h1>Hello :)</h1>
-  );
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+
+    fetch(
+      'https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyA37ZY86iqaX_9MGP4xAP6R0HvI8VrufPk',
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log('error', error));
+  }, []);
+
+  return <VideoList videos={videos} />;
 }
 
 export default App;
